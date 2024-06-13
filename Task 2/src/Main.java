1450b6c3e7455af9f3383b9faed3cdd98d9f4fa0
Licesur <Sovakod@gmail.com>
2024-06-13 17:56:34 +0300

    solved

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        //todo Тут увольняем айтишников
+        int i =0;
+        while (i < 100){
+            i++;
+            if (i % 2 == 1) continue;
+            System.out.println("deemployed " + i);
+        }
     }
 }
